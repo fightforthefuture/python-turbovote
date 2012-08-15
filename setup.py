d119150b9a082c672ab75cc84991217f2b01c8d5
Chuck Harmston <chuck@chuckharmston.com>
2012-08-15 14:48:02 -0500

    Implements find_packages in setup.py to fix a bug when trying to install using pip

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name="python-turbovote",
     version="0.0.1",
     author="Fight for the Future",
     author_email="",
-    packages="turbovote",
+    packages=find_packages(),
     license="LICENSE.txt",
     description="Python wrapper for the TurboVote API",
     long_description=open("README.txt").read(),
