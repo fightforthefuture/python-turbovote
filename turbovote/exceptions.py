a658a96adff1407c2677a97f75016c9b542e4547
Chuck Harmston <chuck@chuckharmston.com>
2012-08-23 11:45:31 -0500

    Removes duplicate error messages from the TurboVote response

--- turbovote/exceptions.py
+++ turbovote/exceptions.py
@@ -1,5 +1,5 @@
 class TurboVoteException(Exception):
     def __init__(self, message, errors):
-        self.errors = errors
+        self.errors = set(errors)
         self.message = ', '.join(self.errors)
         Exception.__init__(self, self.message)
